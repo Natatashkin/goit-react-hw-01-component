a530d3cb7331621ca612b96dbcfa51cd0feb2334
Natatashkin <cs070283sng@gmail.com>
2021-01-10 20:11:20 +0200

    добавила 4 задание и поправила 3е

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Container from './components/Container/Container';
 import Profile from './components/SocialProfile/SocialProfile';
 import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import user from './dataBase/user.json';
 import statisticalData from './dataBase/statistical-data.json';
 import friends from './dataBase/friends.json';
+import transactions from './dataBase/transactions.json';
 
 function App() {
   return (
@@ -29,6 +31,10 @@ function App() {
         <h1>Задание 3</h1>
       </>
       <FriendList friends={friends} />
+      <>
+        <h1>Задание 4</h1>
+      </>
+      <TransactionHistory items={transactions} />
     </Container>
   );
 }
